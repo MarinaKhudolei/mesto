b1bb808bc07fef87271168bade1d48c9ddb634d1
MarinaKhudolei <marina96khu@mail.ru>
2020-08-09 14:36:24 +0300

    Project 6, third iteration

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@
 const changeButton = document.querySelector(".profile__change-button");
 const changeProfileModal = document.querySelector(".modal_type_change-profile");
 const changeProfileCloseButton = changeProfileModal.querySelector(
-  ".modal__close-button"
+    ".modal__close-button"
 );
 
 //change profile form
@@ -13,48 +13,48 @@ const profileProf = document.querySelector(".profile__profession");
 const changeProfileForm = document.querySelector(".form_type_change-profile");
 
 function changeContent(evt) {
-  profileName.textContent = formName.value;
-  profileProf.textContent = formProf.value;
-  closeModal(changeProfileModal);
-  evt.preventDefault();
+    profileName.textContent = formName.value;
+    profileProf.textContent = formProf.value;
+    closeModal(changeProfileModal);
+    evt.preventDefault();
 }
 
 //handle submit button
 const disableButton = (form) => {
-  const buttonSubmit = form.querySelector(".form__submit-button");
-  buttonSubmit.classList.add("form__submit-button_disabled");
-  buttonSubmit.disabled = true;
+    const buttonSubmit = form.querySelector(".form__submit-button");
+    buttonSubmit.classList.add("form__submit-button_disabled");
+    buttonSubmit.disabled = true;
 };
 
 const enableButton = (form) => {
-  const buttonSubmit = form.querySelector(".form__submit-button");
-  buttonSubmit.classList.remove("form__submit-button_disabled");
-  buttonSubmit.disabled = false;
+    const buttonSubmit = form.querySelector(".form__submit-button");
+    buttonSubmit.classList.remove("form__submit-button_disabled");
+    buttonSubmit.disabled = false;
 };
 
 //handle form errors
 const clearForm = (form) => {
-  const inputs = Array.from(form.querySelectorAll(".form__item"));
-  inputs.forEach((inputElement) => {
-    const errorElement = form.querySelector(`#${inputElement.name}-error`);
-    inputElement.classList.remove("form__item_invalid");
-    errorElement.textContent = "";
-    errorElement.classList.remove("form__error_visible");
-  });
+    const inputs = Array.from(form.querySelectorAll(".form__item"));
+    inputs.forEach((inputElement) => {
+        const errorElement = form.querySelector(`#${inputElement.name}-error`);
+        inputElement.classList.remove("form__item_invalid");
+        errorElement.textContent = "";
+        errorElement.classList.remove("form__error_visible");
+    });
 };
 
 //change profile buttons
 changeButton.addEventListener("click", () => {
-  formName.value = profileName.textContent;
-  formProf.value = profileProf.textContent;
-  openModal(changeProfileModal);
+    formName.value = profileName.textContent;
+    formProf.value = profileProf.textContent;
+    openModal(changeProfileModal);
 
-  clearForm(changeProfileForm);
-  enableButton(changeProfileForm);
+    clearForm(changeProfileForm);
+    enableButton(changeProfileForm);
 });
 
 changeProfileCloseButton.addEventListener("click", () => {
-  closeModal(changeProfileModal);
+    closeModal(changeProfileModal);
 });
 
 changeProfileForm.addEventListener("submit", changeContent);
@@ -62,76 +62,76 @@ changeProfileForm.addEventListener("submit", changeContent);
 //create and render cards
 
 const initialCards = [
-  {
-    name: "Архыз",
-    link:
-      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link:
-      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link:
-      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link:
-      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link:
-      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link:
-      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
+    {
+        name: "Архыз",
+        link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    },
+    {
+        name: "Челябинская область",
+        link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+    },
+    {
+        name: "Иваново",
+        link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+    },
+    {
+        name: "Камчатка",
+        link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+    },
+    {
+        name: "Холмогорский район",
+        link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+    },
+    {
+        name: "Байкал",
+        link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    },
 ];
 
 const cardTemplate = document.querySelector("#card").content;
 const cardsSection = document.querySelector(".places");
 
 initialCards.forEach((data) => {
-  renderCard(data);
+    renderCard(data);
 });
 
 //handle card creation
 
 function createCard(data) {
-  const cardElement = cardTemplate.cloneNode(true);
+    const cardElement = cardTemplate.cloneNode(true);
 
-  const cardTitle = cardElement.querySelector(".card__title");
-  const cardImage = cardElement.querySelector(".card__image");
-  cardImage.addEventListener("click", () => {
-    openPopup(data);
-  });
+    const cardTitle = cardElement.querySelector(".card__title");
+    const cardImage = cardElement.querySelector(".card__image");
+    cardImage.addEventListener("click", () => {
+        openPopup(data);
+    });
 
-  const deleteCardButton = cardElement.querySelector(".card__delete-button");
-  deleteCardButton.addEventListener("click", function () {
-    const myCard = deleteCardButton.closest(".places__card");
-    myCard.remove();
-  });
+    const deleteCardButton = cardElement.querySelector(".card__delete-button");
+    deleteCardButton.addEventListener("click", function () {
+        const myCard = deleteCardButton.closest(".places__card");
+        myCard.remove();
+    });
 
-  const likeCardButton = cardElement.querySelector(".card__button");
-  likeCardButton.addEventListener("click", function () {
-    likeCardButton.classList.toggle("card__button_liked");
-  });
+    const likeCardButton = cardElement.querySelector(".card__button");
+    likeCardButton.addEventListener("click", function () {
+        likeCardButton.classList.toggle("card__button_liked");
+    });
 
-  cardTitle.textContent = data.name;
-  cardImage.src = data.link;
-  cardImage.alt = data.name;
+    cardTitle.textContent = data.name;
+    cardImage.src = data.link;
+    cardImage.alt = data.name;
 
-  return cardElement;
+    return cardElement;
 }
 
 function renderCard(data) {
-  cardsSection.prepend(createCard(data));
+    cardsSection.prepend(createCard(data));
 }
 
 //add place modal
@@ -145,22 +145,22 @@ const formImageLink = document.querySelector(".form__item_type_image-link");
 const addPlaceForm = document.querySelector(".form_type_add-place");
 
 function addPlace(evt) {
-  evt.preventDefault();
-  renderCard({ name: formTitle.value, link: formImageLink.value });
-  closeModal(addPlaceModal);
+    evt.preventDefault();
+    renderCard({ name: formTitle.value, link: formImageLink.value });
+    closeModal(addPlaceModal);
 }
 
 addButton.addEventListener("click", () => {
-  openModal(addPlaceModal);
-  formTitle.value = "";
-  formImageLink.value = "";
+    openModal(addPlaceModal);
+    formTitle.value = "";
+    formImageLink.value = "";
 
-  clearForm(addPlaceForm);
-  disableButton(addPlaceForm);
+    clearForm(addPlaceForm);
+    disableButton(addPlaceForm);
 });
 
 addPlaceCloseButton.addEventListener("click", () => {
-  closeModal(addPlaceModal);
+    closeModal(addPlaceModal);
 });
 
 addPlaceForm.addEventListener("submit", addPlace);
@@ -175,63 +175,63 @@ const popupCaption = openPopupImage.querySelector(".popup__caption");
 const popupCloseButton = openPopupImage.querySelector(".popup__close-button");
 
 function openPopup(data) {
-  popup.classList.add("popup-window_opened");
-  document.addEventListener("keydown", popupListener);
-  popup.append(popupContainer);
-  popupCaption.textContent = data.name;
-  popupImage.src = data.link;
-  popupImage.alt = data.name;
+    popup.classList.add("popup-window_opened");
+    document.addEventListener("keydown", popupListener);
+    popup.append(popupContainer);
+    popupCaption.textContent = data.name;
+    popupImage.src = data.link;
+    popupImage.alt = data.name;
 }
 
 function closePopup() {
-  document.removeEventListener("keydown", popupListener);
-  popup.classList.remove("popup-window_opened");
+    document.removeEventListener("keydown", popupListener);
+    popup.classList.remove("popup-window_opened");
 }
 
 popupCloseButton.addEventListener("click", () => {
-  closePopup();
+    closePopup();
 });
 
 //modal escape listener
 function modalListener(evt) {
-  modal = document.querySelector(".modal_opened");
-  if (evt.key === "Escape") {
-    closeModal(modal);
-  }
+    modal = document.querySelector(".modal_opened");
+    if (evt.key === "Escape") {
+        closeModal(modal);
+    }
 }
 
 //popup escape listener
 function popupListener(evt) {
-  if (evt.key === "Escape") {
-    closePopup();
-  }
+    if (evt.key === "Escape") {
+        closePopup();
+    }
 }
 
 //open and close modals
 function openModal(modalWindow) {
-  modalWindow.classList.add("modal_opened");
-  document.addEventListener("keydown", modalListener);
+    modalWindow.classList.add("modal_opened");
+    document.addEventListener("keydown", modalListener);
 }
 
 function closeModal(modalWindow) {
-  modalWindow.classList.remove("modal_opened");
-  document.removeEventListener("keydown", modalListener);
+    modalWindow.classList.remove("modal_opened");
+    document.removeEventListener("keydown", modalListener);
 }
 
 modal = Array.from(document.querySelectorAll(".modal"));
 const handleModalListeners = () => {
-  modal.forEach((modalWindow) => {
-    closeModal(modalWindow);
-  });
-  closePopup();
+    modal.forEach((modalWindow) => {
+        closeModal(modalWindow);
+    });
+    closePopup();
 };
 
 //if overlay is clicked
 document.addEventListener("click", function (evt) {
-  if (
-    evt.target.classList.contains("modal") ||
-    evt.target.classList.contains("popup-window")
-  ) {
-    handleModalListeners();
-  }
+    if (
+        evt.target.classList.contains("modal") ||
+        evt.target.classList.contains("popup-window")
+    ) {
+        handleModalListeners();
+    }
 });
